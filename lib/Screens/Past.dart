966f1fe8adb13990ff11923a2697d145525bd2d7
Nlokuman998 <nironelokumannage234@gmail.com>
2024-03-18 10:49:34 +0530

    Create 'ItemWidget' class
    The cards are displayed in a 'ListTile' widget and the text and drop down menu icon are placed within a 'Row' widget inside of the list tile. When the drop down menu icon is clicked, it returns an alert dialog that displays the data required for each card.

--- lib/Screens/Past.dart
+++ lib/Screens/Past.dart
@@ -24,65 +24,68 @@ class _PastState extends State<Past> {
             ),
           ),
         ),
-        body: Column(children: [
-          Expanded(
-            child: ListView.separated(
-              // Represents the number of cards to be displayed.
-              itemCount: 4,
-              separatorBuilder: (BuildContext context, int index) {
-                return const SizedBox(
-                  // Adjusts the space between each card.
-                  height: 16,
-                );
-              },
-              itemBuilder: (BuildContext context, int index) {
-                String labelText = '';
-                String data = '';
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(children: [
+            Expanded(
+              child: ListView.separated(
+                // Represents the number of cards to be displayed.
+                itemCount: 4,
+                separatorBuilder: (BuildContext context, int index) {
+                  return const SizedBox(
+                    // Adjusts the space between each card.
+                    height: 50,
+                  );
+                },
+                itemBuilder: (BuildContext context, int index) {
+                  String labelText = '';
+                  String data = '';
 
-                switch (index) {
-                  case 0:
-                    labelText = 'Fertilizer Type';
-                    data = 'Retrieve type from database';
-                    break;
+                  switch (index) {
+                    case 0:
+                      labelText = 'Fertilizer Type';
+                      data = 'Retrieve type from database';
+                      break;
 
-                  case 1:
-                    labelText = 'Required Amount';
-                    data = 'Retrieve amount from database';
-                    break;
+                    case 1:
+                      labelText = 'Required Amount';
+                      data = 'Retrieve amount from database';
+                      break;
 
-                  case 2:
-                    labelText = 'Expected Date Of Rain';
-                    data = 'Retrieve date from database';
-                    break;
+                    case 2:
+                      labelText = 'Expected Date Of Rain';
+                      data = 'Retrieve date from database';
+                      break;
 
-                  case 3:
-                    labelText = 'Expected Time Of Rain';
-                    data = 'Retrieve time from database';
-                    break;
-                }
-                return ItemWidget(
-                  labelText: labelText,
-                  data: data,
-                );
-              },
+                    case 3:
+                      labelText = 'Expected Time Of Rain';
+                      data = 'Retrieve time from database';
+                      break;
+                  }
+                  return ItemWidget(
+                    labelText: labelText,
+                    data: data,
+                  );
+                },
+              ),
             ),
-          ),
-          Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Add the functionality to the button so that when pressed, the user will be directed to the current page.
-                    },
-                    child: const Text(
-                      'OK',
-                      style: TextStyle(
-                        color: Colors.black,
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Add the functionality to the button so that when pressed, the user will be directed to the current page.
+                      },
+                      child: const Text(
+                        'OK',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-                  )))
-        ]));
+                    )))
+          ]),
+        ));
   }
 }
 
@@ -94,6 +97,43 @@ class ItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+        // The width is set to 'double.infinity' so that it can take the entire width of it's parent container.
+        width: double.infinity,
+        height: 100,
+        child: Card(
+            child: Center(
+                child: ListTile(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            labelText,
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: Icon(Icons.expand_more),
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text(labelText),
+                            content: Text(data),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Close'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }))));
   }
 }
