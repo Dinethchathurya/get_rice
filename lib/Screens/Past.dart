5865b841f1a494d2a8a66408405f76351a2ee6fe
Nlokuman998 <nironelokumannage234@gmail.com>
2024-03-18 10:32:43 +0530

    Update 'Past.dart'
    Created a column that consists of the layout of the four cards along with an elevated button.

--- lib/Screens/Past.dart
+++ lib/Screens/Past.dart
@@ -11,19 +11,89 @@ class _PastState extends State<Past> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue[200],
-        title: const Center(
-          child: Text(
-            "Past",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[200],
+          title: const Center(
+            child: Text(
+              "Past",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
-      ),
-    );
+        body: Column(children: [
+          Expanded(
+            child: ListView.separated(
+              // Represents the number of cards to be displayed.
+              itemCount: 4,
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(
+                  // Adjusts the space between each card.
+                  height: 16,
+                );
+              },
+              itemBuilder: (BuildContext context, int index) {
+                String labelText = '';
+                String data = '';
+
+                switch (index) {
+                  case 0:
+                    labelText = 'Fertilizer Type';
+                    data = 'Retrieve type from database';
+                    break;
+
+                  case 1:
+                    labelText = 'Required Amount';
+                    data = 'Retrieve amount from database';
+                    break;
+
+                  case 2:
+                    labelText = 'Expected Date Of Rain';
+                    data = 'Retrieve date from database';
+                    break;
+
+                  case 3:
+                    labelText = 'Expected Time Of Rain';
+                    data = 'Retrieve time from database';
+                    break;
+                }
+                return ItemWidget(
+                  labelText: labelText,
+                  data: data,
+                );
+              },
+            ),
+          ),
+          Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Add the functionality to the button so that when pressed, the user will be directed to the current page.
+                    },
+                    child: const Text(
+                      'OK',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  )))
+        ]));
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({super.key, required this.labelText, required this.data});
+
+  final String labelText;
+  final String data;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
