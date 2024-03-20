d0ae533e839b9bec0da9e3e7868ead23877189b3
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2024-03-20 22:40:07 +0530

    Revert "now system can get nearest mechanic location"
    This reverts commit ec88890e99dc5527d498d1abe8a00083cea56ac6.

--- lib/Screens/WeatherCalculateScreen.dart
+++ lib/Screens/WeatherCalculateScreen.dart
@@ -12,7 +12,6 @@ class WeatherCalculateScreen extends StatelessWidget {
       String apiKey = '6f94d27289744596d64417bacd2fc36c';
       double lat = 6.839118;
       double lon = 80.021366;
-      List<String> rainyDates = [];
 
       Uri url = Uri.parse('https://api.openweathermap.org/data/2.5/forecast'
           '?lat=$lat&lon=$lon&appid=$apiKey');
@@ -24,13 +23,9 @@ class WeatherCalculateScreen extends StatelessWidget {
 
       for (var entry in list) {
         var date = entry['dt_txt'];
-        var id = entry['weather'][0]['id'];
-        if (id >= 500 && id <= 504) {
-          rainyDates.add(date);
-        }
+        print('Date: $date');
       }
-      print(
-          'We suggest you to put fertilizer before ${rainyDates.first}. Rainy date/dates : $rainyDates');
+      // print(response.body);
     }
 
     return Scaffold(
