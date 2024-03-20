f5d041ff56ca12ec45c36f7f547a91ba37041e4d
Nlokuman998 <nironelokumannage234@gmail.com>
2024-03-20 09:32:48 +0530

    Update 'Current.dart'
    Created a column that consists of an 'Expanded' widget that defines the number of cards present within the page. An 'ItemWidget' class has also been created that consists of two parameters called 'labelText' and 'data'. It returns a container that consists of the structure of the cards present within the 'Current.dart' page. When the drop down icon is clicked, an alert dialog will be displayed that shows the required data to the user.

--- lib/Screens/Current.dart
+++ lib/Screens/Current.dart
@@ -24,6 +24,120 @@ class _CurrentPageState extends State<CurrentPage> {
           ),
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.separated(
+                itemCount: 4,
+                separatorBuilder: (BuildContext context, int index) {
+                  return const SizedBox(height: 50);
+                },
+                itemBuilder: (BuildContext context, int index) {
+                  // Retrieve data from the database for each card
+                  String labelText = '';
+                  String data = '';
+
+                  switch (index) {
+                    case 0:
+                      labelText = 'Status';
+                      data = 'Retrieve from database';
+                      break;
+                    case 1:
+                      labelText = 'Date';
+                      data = 'Retrieve from database';
+                      break;
+                    case 2:
+                      labelText = 'Time';
+                      data = 'Retrieve from database';
+                      break;
+                    case 3:
+                      labelText = 'Recorded Data';
+                      data = 'Retrieve from database';
+                      break;
+                  }
+
+                  return ItemWidget(
+                    labelText: labelText,
+                    data: data,
+                  );
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add the functionality to the elevated button.
+                  },
+                  child: Text('OK'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    Key? key,
+    required this.labelText,
+    required this.data,
+  }) : super(key: key);
+
+  final String labelText;
+  final String data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // The width of the card is set to 'double.infinity' to allow the card to take the full width of it's parent container.
+      width: double.infinity,
+      // 'height' property is used to adjust the height of the card.
+      height: 100,
+      child: Card(
+        child: Center(
+          child: ListTile(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Text(
+                    labelText,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ],
+            ),
+            trailing: Icon(Icons.expand_more),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text(labelText),
+                    content: Text(data),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Close'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
