1ca8c9eed6a78aa6309c115594501a9402e99fc9
Nlokuman998 <nironelokumannage234@gmail.com>
2024-03-20 08:53:02 +0530

    Create 'Current.dart'
    Created the current page where the readings taken from the sensors will be displayed.

---
+++ lib/Screens/Current.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CurrentPage extends StatefulWidget {
+  const CurrentPage({super.key});
+
+  @override
+  State<CurrentPage> createState() => _CurrentPageState();
+}
+
+class _CurrentPageState extends State<CurrentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[200],
+        title: const Center(
+          child: Text(
+            "Current",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
