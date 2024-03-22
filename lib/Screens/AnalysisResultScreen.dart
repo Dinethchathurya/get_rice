8389265b134f105bf0d97f28eb80bbd39481db97
AshiniGunawardhana <ashinigunawardhana@gmail.com>
2024-03-23 00:06:11 +0530

    Begin to create Result Screen
    I had ready environment to create result screen.

---
+++ lib/Screens/AnalysisResultScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class resultScreen extends StatelessWidget {
+  const resultScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'Screens/FertilizerCalculateScreen.dart';
 import 'Screens/LoginScreen.dart';
 import 'Screens/RegisterScreen.dart';
 import 'Screens/WeatherCalculateScreen.dart';
-
+import 'Screens/AnalysisResultScreen.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,12 +15,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light(),
-      initialRoute: '/WeatherCalculateScreen',
+      initialRoute: '/result',
       routes: {
         '/': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
         '/FertilizerCalculateScreen': (context) => FertilizerCalculateScreen(),
         '/WeatherCalculateScreen': (context) => WeatherCalculateScreen(),
+         '/result': (context) => resultScreen(),
       },
     );
   }
