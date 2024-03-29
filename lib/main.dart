393bd9b012029f899ab3b3832acc370e9976567c
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2024-03-29 17:46:31 +0530

    ready to merge

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light(),
-      initialRoute: '/WeatherCalculateScreen',
+      initialRoute: '/',
       routes: {
         '/': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
