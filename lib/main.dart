d040600edcb3e94fee9b1d7abb9eb8012af55eae
AshiniGunawardhana <ashinigunawardhana@gmail.com>
2024-03-30 19:17:04 +0530

    Revert "start building bottom sheet"
    This reverts commit e52f16db9b9ec26e5b7d87e6f0386139bed9fe09.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'Screens/FertilizerCalculateScreen.dart';
 import 'Screens/LoginScreen.dart';
 import 'Screens/RegisterScreen.dart';
 import 'Screens/WeatherCalculateScreen.dart';
-import 'Screens/bottomsheet.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,13 +15,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light(),
-      initialRoute: '/bottomsheet',
+      initialRoute: '/',
       routes: {
         '/': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
         '/FertilizerCalculateScreen': (context) => FertilizerCalculateScreen(),
         '/WeatherCalculateScreen': (context) => WeatherCalculateScreen(),
-        '/bottomsheet' : (context) => BottomSheetForPastData(),
       },
     );
   }
