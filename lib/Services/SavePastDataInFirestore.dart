a97ce4f3cba3845b1f51851b16232c3d3e01e2d5
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2024-03-31 18:26:54 +0530

    create a dart file for save past data in firebase

---
+++ lib/Services/SavePastDataInFirestore.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class GetPastData extends StatelessWidget {
+  const GetPastData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            //
+          },
+          child: Text('kjbhksd'),
+        ),
+      ),
+    );
+  }
+}
