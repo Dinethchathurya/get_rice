0402ab70a23060d7f2f2bf97f7619bbfcaf322aa
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2024-03-31 23:17:20 +0530

    create sign in and register

--- lib/Screens/RegisterScreen.dart
+++ lib/Screens/RegisterScreen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -5,6 +6,93 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final auth = FirebaseAuth.instance;
+    late String email;
+    late String password;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registration Screen'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Register',
+            style: TextStyle(
+              fontSize: 35,
+              color: Colors.teal,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 30),
+            child: Form(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {
+                        email = value;
+                      },
+                      validator: (value) {
+                        return value!.isEmpty ? 'Please enter email' : null;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText:
+                          true, // Set obscureText to true for password field
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter password',
+                        prefixIcon: Icon(Icons
+                            .lock), // Changed icon to a lock icon for password
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {
+                        password = value;
+                      },
+                      validator: (value) {
+                        return value!.isEmpty ? 'Please enter password' : null;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 35),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      onPressed: () async {
+                        await auth.createUserWithEmailAndPassword(
+                            email: email, password: password);
+                        Navigator.pushNamed(context, '/testhome');
+                      },
+                      color: Colors.teal,
+                      textColor: Colors.white,
+                      child: const Text('Register'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'Screens/FertilizerCalculateScreen.dart';
 import 'Screens/LoginScreen.dart';
 import 'Screens/RegisterScreen.dart';
 import 'Screens/WeatherCalculateScreen.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -13,15 +19,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/register': (context) => RegisterScreen(),
-        '/FertilizerCalculateScreen': (context) => FertilizerCalculateScreen(),
-        '/WeatherCalculateScreen': (context) => WeatherCalculateScreen(),
-      },
-    );
+    return MaterialApp(theme: ThemeData.light(), initialRoute: '/', routes: {
+      '/': (context) => LoginScreen(),
+      '/register': (context) => RegisterScreen(),
+      '/FertilizerCalculateScreen': (context) => FertilizerCalculateScreen(),
+      '/WeatherCalculateScreen': (context) => WeatherCalculateScreen(),
+      '/testhome': (context) => TestHome(),
+    });
   }
 }
