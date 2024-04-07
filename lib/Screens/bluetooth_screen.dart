a68495f1b25e3a868b343f8f369a37d88fca04e7
dushan6693 <dushanmadushankabeligala9@gmail.com>
2024-04-07 23:38:20 +0530

    bluetooth update 2.0
    files moved and some codes changed

---
+++ lib/Screens/bluetooth_screen.dart
@@ -0,0 +1,303 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+class BluetoothScreen extends StatefulWidget {
+  const BluetoothScreen({super.key});
+
+  @override
+  State<BluetoothScreen> createState() => _BluetoothScreenState();
+}
+
+class _BluetoothScreenState extends State<BluetoothScreen> {
+  final FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
+  late BluetoothConnection _connection;
+  bool _connected = false;
+  late BluetoothDevice _connectedDevice;
+  final TextEditingController _typedText = TextEditingController();
+
+  // String _dataReceived = '{"nitro": "0","pos": "0","pota": "0", "ph": "0"}';
+  String _nitro = "0";
+  String _pos = "0";
+  String _pota = "0";
+  String _ph = "0";
+
+  @override
+  void initState() {
+    super.initState();
+    _isBluetoothEnabled();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade400,
+        foregroundColor: Colors.white,
+        title: const Text("BT Serial"),
+        elevation: 4.0,
+        shadowColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 80.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        _connected ? 'Connected' : 'Disconnected',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(30.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          FilledButton(
+                            onPressed: _connected
+                                ? _disconnectFromDevice
+                                : _showDeviceListPopup,
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(
+                                    Colors.blue.shade400),
+                                foregroundColor:
+                                    MaterialStateProperty.all(Colors.white)),
+                            child: Text(
+                                _connected ? 'Disconnect' : 'Select Device'),
+                          )
+                        ],
+                      ),
+                      if (_connected)
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text("Device: ${_connectedDevice.name}"),
+                            Text(
+                              _connectedDevice.address.toString(),
+                              style: const TextStyle(
+                                  fontSize: 12, color: Colors.black38),
+                            )
+                          ],
+                        )
+                    ],
+                  ),
+                ),
+
+                Container(
+                  margin: const EdgeInsets.all(30.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          FilledButton(
+                            onPressed: () {
+                              _connected ? _sendData(data: "5") : null;
+                            },
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(
+                                    Colors.blue.shade400),
+                                foregroundColor:
+                                    MaterialStateProperty.all(Colors.white)),
+                            child: Text("Read Data"),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(right: 20.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Nitrogen: ",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                Text(
+                                  "Phosphorus: ",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                Text(
+                                  "Potassium: ",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                Text(
+                                  "PH: ",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                _nitro,
+                                style: const TextStyle(fontSize: 20.0),
+                              ),
+                              Text(
+                                _pos,
+                                style: const TextStyle(fontSize: 20.0),
+                              ),
+                              Text(
+                                _pota,
+                                style: const TextStyle(fontSize: 20.0),
+                              ),
+                              Text(
+                                _ph,
+                                style: const TextStyle(fontSize: 20.0),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _isBluetoothEnabled() async {
+    super.initState();
+    bool? isAvailable = await _bluetooth.isAvailable;
+
+    if (isAvailable ?? false) {
+      // Check if Bluetooth is turned on
+      bool? isEnabled = await _bluetooth.isEnabled;
+
+      if (!isEnabled! ?? false) {
+        // Request to turn on Bluetooth
+        await _bluetooth.requestEnable();
+      }
+    } else {
+      print('Bluetooth is not available on this device.');
+    }
+  }
+
+  Future<void> _showDeviceListPopup() async {
+    _bluetooth.cancelDiscovery(); // Cancel any ongoing discovery
+
+    List<BluetoothDevice> devices = [];
+
+    try {
+      devices = await _bluetooth.getBondedDevices();
+    } catch (ex) {
+      print("Error getting bonded devices: $ex");
+    }
+
+    if (devices.isEmpty) {
+      print("No bonded devices available");
+      return;
+    }
+
+    BluetoothDevice selectedDevice = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Select a device'),
+          content: Column(
+            children: <Widget>[
+              for (BluetoothDevice device in devices)
+                ListTile(
+                  title: Text(device.name.toString()),
+                  subtitle: Text(device.address),
+                  onTap: () {
+                    Navigator.pop(context, device);
+                  },
+                ),
+            ],
+          ),
+        );
+      },
+    );
+
+    if (selectedDevice != null) {
+      await _connectToDevice(selectedDevice);
+    }
+  }
+
+  Future<void> _connectToDevice(BluetoothDevice device) async {
+    try {
+      BluetoothConnection connection =
+          await BluetoothConnection.toAddress(device.address);
+      print("Connected to ${device.name}");
+
+      // Start listening for incoming data
+      connection.input?.listen(
+        (Uint8List data) {
+          String receivedData = utf8.decode(data);
+          print("Received data: $receivedData");
+
+          setState(() {
+            _nitro = _getJson(receivedData, "nitro");
+            _pos = _getJson(receivedData, "pos");
+            _pota = _getJson(receivedData, "pota");
+            _ph = _getJson(receivedData, "ph");
+          });
+        },
+        onDone: () {
+          print("Disconnected remotely!");
+          _disconnectFromDevice();
+        },
+        onError: (error) {
+          print("Error receiving data: $error");
+        },
+      );
+
+      setState(() {
+        _connected = true;
+        _connection = connection;
+        _connectedDevice = device;
+      });
+    } catch (ex) {
+      print("Error connecting to device: $ex");
+    }
+  }
+
+  void _disconnectFromDevice() {
+    if (_connection != null) {
+      _connection.finish();
+      setState(() {
+        _connected = false;
+      });
+    }
+  }
+
+  void _sendData({required String data}) {
+    if (_connection != null) {
+      data = "$data\n";
+      _connection.output.add(Uint8List.fromList(data.codeUnits));
+      _connection.output.allSent.then((_) {
+        print("Data sent successfully");
+      });
+    }
+  }
+
+  _getJson(String data, String name) {
+    try {
+      Map<String, dynamic> jsonData = jsonDecode(data);
+      String value = jsonData[name];
+      print(value);
+      return value;
+    } on FormatException catch (e) {
+      print("Invalid JSON string. $e");
+    }
+    return '-';
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
-import 'package:get_rice/Services/Bluetooth/bluetooth_manager.dart';
+import 'package:get_rice/Screens/bluetooth_screen.dart';
 import 'package:provider/provider.dart';
-
 import 'Screens/BottomSheet.dart';
-
 import 'package:get_rice/Screens/Current.dart';
 import 'package:get_rice/Screens/Past.dart';
 
@@ -72,7 +70,7 @@ class MyApp extends StatelessWidget {
                 FertilizerCalculateScreen(),
             '/WeatherCalculateScreen': (context) => WeatherCalculateScreen(),
             '/testhome': (context) => TestHome(),
-            '/testBluetooth': (context) => BluetoothHome(),
+            '/testBluetooth': (context) => BluetoothScreen(),
             '/bottomSheet': (context) => BottomSheetWidget(),
             '/result': (context) => resultScreen(),
             'CurrentPage': (context) => CurrentPage(),
@@ -128,217 +126,5 @@ class TestHome extends StatelessWidget {
     );
   }
 }
-class BluetoothHome extends StatefulWidget {
-  const BluetoothHome({super.key});
 
-  @override
-  State<BluetoothHome> createState() => _BluetoothHomeState();
-}
-//this class for test bluetooth part
-//dushan
-class _BluetoothHomeState extends State<BluetoothHome> {
-  BluetoothManager bluetoothManager = BluetoothManager();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade400,
-        foregroundColor: Colors.white,
-        title: const Text("BT Serial"),
-        elevation: 4.0,
-        shadowColor: Colors.black,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: const EdgeInsets.only(bottom: 80.0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 40.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        _connected ? 'Connected' : 'Disconnected',
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(30.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          FilledButton(
-                            onPressed: _connected
-                                ? bluetoothManager.disconnectFromDevice
-                                : _showDeviceListPopup,
-                            style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    Colors.blue.shade400),
-                                foregroundColor:
-                                MaterialStateProperty.all(Colors.white)),
-                            child: Text(
-                                _connected ? 'Disconnect' : 'Select Device'),
-                          )
-                        ],
-                      ),
-                      if (_connected)
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text("Device: ${_connectedDevice.name}"),
-                            Text(
-                              _connectedDevice.address.toString(),
-                              style: const TextStyle(
-                                  fontSize: 12, color: Colors.black38),
-                            )
-                          ],
-                        )
-                    ],
-                  ),
-                ),
-
-                Container(
-                  margin: const EdgeInsets.all(30.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          FilledButton(
-                            onPressed: () {
-                              _connected ? _sendData("5") : null;
-                            },
-                            style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    Colors.blue.shade400),
-                                foregroundColor:
-                                MaterialStateProperty.all(Colors.white)),
-                            child: Text("Read Data"),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.only(right: 20.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Nitrogen: ",
-                                  style: TextStyle(fontSize: 20.0),
-                                ),
-                                Text(
-                                  "Phosphorus: ",
-                                  style: TextStyle(fontSize: 20.0),
-                                ),
-                                Text(
-                                  "Potassium: ",
-                                  style: TextStyle(fontSize: 20.0),
-                                ),
-                                Text(
-                                  "PH: ",
-                                  style: TextStyle(fontSize: 20.0),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                bluetoothManager.nitro,
-                                style: const TextStyle(fontSize: 20.0),
-                              ),
-                              Text(
-                                bluetoothManager.pos,
-                                style: const TextStyle(fontSize: 20.0),
-                              ),
-                              Text(
-                                bluetoothManager.pota,
-                                style: const TextStyle(fontSize: 20.0),
-                              ),
-                              Text(
-                                bluetoothManager.ph,
-                                style: const TextStyle(fontSize: 20.0),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ]),
-        ),
-      ),
-    );
-  }
-  //all the functions are there to read bluetooth values.
-  //to read all values pass number 5(String) using _sendData() function
-  //then, received data print on display.
-  //no need to call function to read data.
-
-  @override
-  void initState() {
-    super.initState();
-    bluetoothManager.isBluetoothEnabled();
-  }
-  void _sendData(String data){
-    bluetoothManager.sendData(data: data);
-  }
-  bool get _connected => bluetoothManager.connected;
-  BluetoothDevice get _connectedDevice=>bluetoothManager.connectedDevice;
-  FlutterBluetoothSerial get bluetooth => bluetoothManager.bluetooth;
-
-  Future<void> _showDeviceListPopup() async {
-    bluetooth.cancelDiscovery(); // Cancel any ongoing discovery
-
-    List<BluetoothDevice> devices = [];
-
-    try {
-      devices = await bluetooth.getBondedDevices();
-    } catch (ex) {
-      print("Error getting bonded devices: $ex");
-    }
-
-    if (devices.isEmpty) {
-      print("No bonded devices available");
-      return;
-    }
-
-    BluetoothDevice selectedDevice = await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Select a device'),
-          content: Column(
-            children: <Widget>[
-              for (BluetoothDevice device in devices)
-                ListTile(
-                  title: Text(device.name.toString()),
-                  subtitle: Text(device.address),
-                  onTap: () {
-                    Navigator.pop(context, device);
-                  },
-                ),
-            ],
-          ),
-        );
-      },
-    );
-
-    if (selectedDevice != null) {
-      await bluetoothManager.connectToDevice(selectedDevice);
-    }
-  }
-
-
-}
 
